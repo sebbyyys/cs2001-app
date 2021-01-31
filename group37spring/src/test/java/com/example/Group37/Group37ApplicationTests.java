6d58ce10e87016a8c8be13b5c1f549b1b1a8eb2b
Iulius Cernautan <1907889@brunel.ac.uk>
2021-01-31 13:46:44 +0000

    Set up of the Spring Boot Framework

---
+++ group37spring/src/test/java/com/example/Group37/Group37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Group37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
